3538dd712036ce1e095e8766011fad7ed696375f
driftingS13 <christhorney@gmail.com>
2023-07-06 16:38:15 +1000

    added project
    added postcode tool project

---
+++ postcode-tool/postcode.js
@@ -0,0 +1,18 @@
+function compute() {
+    // Get the input values
+    var list1 = document.querySelector(".list1").value;
+    var list2 = document.querySelector(".list2").value;
+  
+    // Convert the input lists to arrays
+    var array1 = list1.split(/[, ]+/);
+    var array2 = list2.split(/[, ]+/);
+  
+    // Remove duplicates from array1 that exist in array2
+    var result = array1.filter(item => !array2.includes(item));
+  
+    // Convert the resulting array to the desired format
+    var output = result.join(", ");
+  
+    // Set the value of the output field
+    document.querySelector(".list3").value = output;
+  }
