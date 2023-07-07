9200a5bbaa9b1726e023a1b3467006a8d9d85179
driftingS13 <christhorney@gmail.com>
2023-07-07 13:52:24 +1000

    addedfuntions
    added funtions to postcode tool to make woocommerce compliant list output

--- postcode-tool/postcode.js
+++ postcode-tool/postcode.js
@@ -16,3 +16,50 @@ function compute() {
     // Set the value of the output field
     document.querySelector(".list3").value = output;
   }
+
+  function wooLayout() {
+    // Get the input value
+    var list4 = document.querySelector(".list4").value;
+  
+    // Convert the input list to an array
+    var array4 = list4.split(/[, ]+/);
+  
+    // Generate the desired output format
+    var output2 = array4.join("\n");
+  
+    // Set the value of the output field
+    document.querySelector(".list5").value = output2;
+  }
+
+  function copyList3() {
+    var list3 = document.querySelector(".list3");
+    list3.select();
+    document.execCommand("copy");
+  }
+  
+  function copyList5() {
+    var list5 = document.querySelector(".list5");
+    list5.select();
+    document.execCommand("copy");
+  }
+  
+  function pasteList1() {
+    var list1 = document.querySelector(".list1");
+    navigator.clipboard.readText().then(function (text) {
+      list1.value = text;
+    });
+  }
+  
+  function pasteList2() {
+    var list2 = document.querySelector(".list2");
+    navigator.clipboard.readText().then(function (text) {
+      list2.value = text;
+    });
+  }
+  
+  function pasteList4() {
+    var list4 = document.querySelector(".list4");
+    navigator.clipboard.readText().then(function (text) {
+      list4.value = text;
+    });
+  }
